0a876c9dc0249e28fc3bce204cd1b73c0250180f
Daniel Andersen <dan.musik@gmail.com>
2021-09-13 08:17:45 -0300

    eureka server

---
+++ src/test/java/com/vnoxiaene/gokustore/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnoxiaene.gokustore.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
